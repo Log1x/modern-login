29c58fea7bbbbe63883f80b3d8f8d018bda40046
Brandon <brandon@tendency.me>
2020-09-11 07:40:00 -0500

    fix(css): Fix trim-alt-invert variable name (Fixes #3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@ module.exports = {
   theme: {
     extend: {
       fontSize: {
-        'none': '0',
+        none: '0',
       },
       colors: {
         brand: {
@@ -13,22 +13,25 @@ module.exports = {
           default: 'var(--login-trim, #191919)',
           alt: 'var(--login-trim-alt, #282828)',
           invert: 'var(--login-trim-invert, #fff)',
-          'alt-invert': 'var(--login-alt-invert, #fff)',
+          'alt-invert': 'var(--login-trim-alt-invert, #fff)',
         },
-      }
-    }
+      },
+    },
   },
   variants: {},
   plugins: [
     ({ addUtilities }) => {
-      addUtilities({
-        '.indent-none': {
-          'text-indent': '0',
+      addUtilities(
+        {
+          '.indent-none': {
+            'text-indent': '0',
+          },
+          '.bg-none': {
+            'background-image': 'none',
+          },
         },
-        '.bg-none': {
-          'background-image': 'none',
-        },
-      }, ['responsive'])
-    }
-  ]
-}
+        ['responsive']
+      );
+    },
+  ],
+};
